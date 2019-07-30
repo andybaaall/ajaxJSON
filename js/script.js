d9ea89b9e91519e20194e733b94592c2249308e5
Andy Ball <932400399@bun2-206-11.local>
2019-07-31 10:50:15 +1200

    really rough render of mock data

--- js/script.js
+++ js/script.js
@@ -2,17 +2,46 @@
 
 $(document).ready(function(){
 
+  let firstName;
+  let lastName;
+  let email;
+  let gender;
+  let birthYear;
+  let city;
+  let idealBreakfast;
+
   const btn = $('#getPeople');
   btn.click(function(){
     // console.log('got a click');
     $.ajax({
       // ajax requests need the five following parameters:
       type: 'GET',
-      url: 'people.json',
+      url: 'data/MOCK_DATA.json',
       dataType: 'json',
       success: function(data){
-        console.log('success');
-        console.log(data);
+        for (var i = 0; i < 10; i++) {
+          firstName = data[i].first_name;
+          lastName = data[i].last_name;
+          email = data[i].email;
+          gender = data[i].gender;
+          birthYear = data[i].birth_year;
+          city = data[i].city;
+          idealBreakfast = data[i].ideal_breakfast;
+
+          // const cardContainer = $('#cardContainer');
+          // cardContainer.innerHTML += 'hello world';
+
+          let cardContainer = document.getElementById('cardContainer');
+          cardContainer.innerHTML += `<div class="card m-2">
+                                        <div class="card-body">
+                                          <h4 class="card-title">${firstName} ${lastName}</h4>
+                                          <div class="card-text"> birth year : ${birthYear}</div>
+                                          <div class="card-text"> ideal breakfast : ${idealBreakfast}</div>
+                                        </div>
+                                      </div>`;
+
+
+        }
       },
       error: function(error){
         // bad URL, no connection, bad request, errors in json file ...
@@ -21,5 +50,6 @@ $(document).ready(function(){
       }
     })
   })
+
 
 });
